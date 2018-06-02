4b50bafecd0eb5feacab3a96607e7e42e45ba577
durdenx <durdenx@users.noreply.github.com>
2018-06-02 13:35:54 +0200

    change const to var

--- index.js
+++ index.js
@@ -19,7 +19,7 @@
 // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
 // USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-const Emitter = require("emitter-component");
+var Emitter = require("emitter-component");
 
 function Stream() {
   Emitter.call(this);
